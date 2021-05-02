8acbcbf33c57aae12859eb0c5716b84805fba6ba
Youssef Ahmed <youssef9300@hotmail.com>
2021-05-02 07:57:42 +0200

    Added the circularScan to the disk Scheduling

--- src/main.java
+++ src/main.java
@@ -2,59 +2,61 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class node{
-    boolean accessed=false;
+class node {
+    boolean accessed = false;
     String distance;
 }
+
 public class main {
 
-    public static void FCFS (String[] q,int p){
+    public static void FCFS(String[] q, int p) {
         System.out.println("FCFS algorithm sequence:........");
-        System.out.print(p+"->");
-        int sum=0;
-        int point=0;
-        int back=p;
-        for(int i=0;i<q.length;i++){
+        System.out.print(p + "->");
+        int sum = 0;
+        int point = 0;
+        int back = p;
+        for (int i = 0; i < q.length; i++) {
             System.out.print(q[i]);
-            point=Math.abs(Integer.parseInt(q[i])-back);
-            back=Integer.parseInt(q[i]);
-            sum+=point;
-            if(i<q.length-1)
+            point = Math.abs(Integer.parseInt(q[i]) - back);
+            back = Integer.parseInt(q[i]);
+            sum += point;
+            if (i < q.length - 1)
                 System.out.print("->");
             else
                 System.out.println("");
         }
-        System.out.println("The total head movement: "+sum+" Cylinders.");
+        System.out.println("The total head movement: " + sum + " Cylinders.");
         System.out.println("-------------------------------------------------");
     }
-    public static void SCAN (String[] q,int p){
+
+    public static void SCAN(String[] q, int p) {
         System.out.println("SCAN algorithm sequence:........");
-        System.out.print(p+"->");
-        int sum=0;
-        int point=0;
-        int back=p;
+        System.out.print(p + "->");
+        int sum = 0;
+        int point = 0;
+        int back = p;
         int size = q.length;
-        int [] arr = new int [size];
-        for(int i=0; i<size; i++) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
             arr[i] = Integer.parseInt(q[i]);
         }
         Arrays.sort(arr);
-        int next=0;
-        int idx=0;
-        int counter=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]<p) {
+        int next = 0;
+        int idx = 0;
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < p) {
                 next = arr[i];
-                idx=i;
-                counter=i;
+                idx = i;
+                counter = i;
             }
         }
-        boolean flag=true;
-        for(int i=0;i<q.length;i++){
+        boolean flag = true;
+        for (int i = 0; i < q.length; i++) {
             System.out.print(next);
-            point=Math.abs(next-back);
-            back=next;
-            if(i<q.length-1) {
+            point = Math.abs(next - back);
+            back = next;
+            if (i < q.length - 1) {
                 if (flag) {
                     counter--;
                     next = arr[counter];
@@ -62,93 +64,162 @@ public class main {
                     next = arr[counter++];
                 }
             }
-            if(counter<1){
-                counter=idx+1;
-                flag=false;
+            if (counter < 1) {
+                counter = idx + 1;
+                flag = false;
             }
-            sum+=point;
-            if(i<q.length-1)
+            sum += point;
+            if (i < q.length - 1)
                 System.out.print("->");
             else
                 System.out.println("");
         }
-        System.out.println("The total head movement: "+sum+" Cylinders.");
+        System.out.println("The total head movement: " + sum + " Cylinders.");
         System.out.println("-------------------------------------------------");
     }
-    public static int findminSSTF(ArrayList<node>queue, int head)
-    {
-        ArrayList<Integer>m = new ArrayList<Integer>();
-        for (int i = 0; i < queue.size(); i++){
-                if(queue.get(i).accessed!=true) {
-                    m.add(Math.abs(Integer.parseInt(queue.get(i).distance) - head));
-                }
-                else
-                    m.add(-1);
+
+    public static int findminSSTF(ArrayList<node> queue, int head) {
+        ArrayList<Integer> m = new ArrayList<Integer>();
+        for (int i = 0; i < queue.size(); i++) {
+            if (queue.get(i).accessed != true) {
+                m.add(Math.abs(Integer.parseInt(queue.get(i).distance) - head));
+            } else
+                m.add(-1);
         }
-        int min=0;
-        for(int i=0;i<m.size();i++) {
-            if(m.get(i)!=-1){
-                min=m.get(i);
+        int min = 0;
+        for (int i = 0; i < m.size(); i++) {
+            if (m.get(i) != -1) {
+                min = m.get(i);
                 break;
             }
         }
-        int idx=-1;
-        for(int i=0;i<m.size();i++){
-            if(m.get(i)<min&&m.get(i)!=-1) {
-                min=m.get(i);
-                idx=m.indexOf(min);
+        int idx = -1;
+        for (int i = 0; i < m.size(); i++) {
+            if (m.get(i) < min && m.get(i) != -1) {
+                min = m.get(i);
+                idx = m.indexOf(min);
             }
         }
-        if(idx==-1)
-            idx=m.indexOf(min);
+        if (idx == -1)
+            idx = m.indexOf(min);
         return idx;
     }
 
-    public static void SSTF (ArrayList<node>q,int p){
+    public static void SSTF(ArrayList<node> q, int p) {
         System.out.println("SSTF algorithm sequence:........");
-        System.out.print(p+"->");
-        int sum=0;
-        int point=0;
-        int back=p;
+        System.out.print(p + "->");
+        int sum = 0;
+        int point = 0;
+        int back = p;
         int req;
-        for(int i=0;i<q.size();i++){
-            req=Integer.parseInt(q.get(findminSSTF(q,back)).distance);
-            q.get(findminSSTF(q,back)).accessed=true;
+        for (int i = 0; i < q.size(); i++) {
+            req = Integer.parseInt(q.get(findminSSTF(q, back)).distance);
+            q.get(findminSSTF(q, back)).accessed = true;
             System.out.print(req);
-            point=Math.abs(req-back);
-            back=req;
-            sum+=point;
-            if(i<q.size()-1)
+            point = Math.abs(req - back);
+            back = req;
+            sum += point;
+            if (i < q.size() - 1)
                 System.out.print("->");
             else
                 System.out.println("");
 
         }
-        System.out.println("The total head movement: "+sum+" Cylinders.");
+        System.out.println("The total head movement: " + sum + " Cylinders.");
         System.out.println("-------------------------------------------------");
     }
 
-    public static void main(String args[]){
+    public static void circularScan(String[] q, int p) {
+        System.out.println("C-SCAN algorithm sequence:........");
+        System.out.print(p + "->");
+        int sum = 0;
+        int size = q.length;
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = Integer.parseInt(q[i]);
+        }
+        Arrays.sort(arr);
+        int max = roundUp(arr[arr.length - 1], 100) - 1;
+
+        int index;
+        for (index = 0; index < arr.length; index++) {
+            if (p < arr[index]) {
+                break;
+            }
+        }
+        if (index == arr.length) {
+            index = 0;
+        }
+
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[arr.length - 1] < p && i == 0) {
+                System.out.print("0" + "->");
+            }
+
+            System.out.print(arr[index]);
+
+            if (i + 1 != arr.length) {
+                System.out.print("->");
+            }
+            if (index != 0) {
+                if (i == 0) {
+                    sum += arr[index] - p;
+                } else {
+                    sum += arr[index] - arr[index - 1];
+                }
+            } else {
+                sum += arr[index];
+            }
+            if (index + 1 == arr.length) {
+                if (i + 1 != arr.length) {
+                    if (i + 1 == arr.length) {
+                        System.out.print("->");
+                    }
+                    System.out.print(max + "->" + "0");
+                    System.out.print("->");
+                }
+
+                sum += max - arr[index];
+                index = 0;
+                sum += max;
+            } else {
+                index++;
+            }
+            i++;
+        }
+        System.out.println();
+        System.out.println("The total head movement: " + sum + " Cylinders.");
+        System.out.println("-------------------------------------------------");
+
+    }
+
+    static int roundUp(int n, int approximate) {
+        int a = (n / approximate) * approximate;
+
+        int b = a + approximate;
+
+        return b;
+    }
+
+    public static void main(String args[]) {
 
         System.out.println("Enter input queue:");
-        Scanner m= new Scanner(System.in);
-        String input=m.nextLine();
+        Scanner m = new Scanner(System.in);
+        String input = m.nextLine();
         String[] queue = input.split(",");
-        ArrayList<node>q=new ArrayList<>();
-        for(int i=0;i<queue.length;i++){
-            node n=new node();
-            n.distance=queue[i];
+        ArrayList<node> q = new ArrayList<>();
+        for (int i = 0; i < queue.length; i++) {
+            node n = new node();
+            n.distance = queue[i];
             q.add(n);
         }
         System.out.println("Enter Head pointer:");
-        int pointer=m.nextInt();
-        FCFS(queue,pointer);
-        SSTF(q,pointer);
-        SCAN(queue,pointer);
-
-
-
-
+        int pointer = m.nextInt();
+        FCFS(queue, pointer);
+        SSTF(q, pointer);
+        //SCAN(queue, pointer);
+        circularScan(queue, pointer);
 
 
     }
